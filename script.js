2e0e6b3b5aabea2b2792e9668f32224507b53cb0
colinnnnnnnnnnn <kalinpoiata@gmail.com>
2023-03-29 19:26:46 +0300

    Add all buttons and query selectors brute force def wasnt annoying at all

---
+++ script.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+const one = document.querySelector('#1');
+const two = document.querySelector('#2');
+const three = document.querySelector('#3');
+const four = document.querySelector('#4');
+const five = document.querySelector('#5');
+const six = document.querySelector('#6');
+const seven = document.querySelector('#7');
+const eight = document.querySelector('#8');
+const nine = document.querySelector('#9');
+const zero = document.querySelector('#0');
+const plus = document.querySelector('#+');
+const minus = document.querySelector('#-');
+const times = document.querySelector('#*');
+const over = document.querySelector('#/');
+const equals = document.querySelector('#=');
+
+function operate(sign, a, b) {
+
+}
