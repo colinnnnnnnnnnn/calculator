4721495cdde18827d141e2ff2e48efbd1f24c2ad
colinnnnnnnnnnn <kalinpoiata@gmail.com>
2023-03-29 19:47:16 +0300

    Realise that i can do it all autocatically which is much better, add way to input a number

--- script.js
+++ script.js
@@ -14,21 +14,18 @@ function divide(a, b) {
     return a / b;
 }
 
-const one = document.querySelector('#1');
-const two = document.querySelector('#2');
-const three = document.querySelector('#3');
-const four = document.querySelector('#4');
-const five = document.querySelector('#5');
-const six = document.querySelector('#6');
-const seven = document.querySelector('#7');
-const eight = document.querySelector('#8');
-const nine = document.querySelector('#9');
-const zero = document.querySelector('#0');
-const plus = document.querySelector('#+');
-const minus = document.querySelector('#-');
-const times = document.querySelector('#*');
-const over = document.querySelector('#/');
-const equals = document.querySelector('#=');
+const numButtons = document.querySelectorAll('#number');
+const operButtons = document.querySelectorAll('#operator');
+
+let firstNum = 0;
+
+numButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        firstNum *= 10;
+        firstNum += Number(button.textContent);
+        console.log(firstNum);
+    })
+})
 
 function operate(sign, a, b) {
 
