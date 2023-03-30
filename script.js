f51d99e8a3bcf65039f997d25b51f53eda53aec3
colinnnnnnnnnnn <kalinpoiata@gmail.com>
2023-03-30 19:02:48 +0300

    Add system so that the valus that the user enters are stored properly

--- script.js
+++ script.js
@@ -14,19 +14,80 @@ function divide(a, b) {
     return a / b;
 }
 
+function readNum(num, button) {
+    num *= 10;
+    num += Number(button.textContent);
+    return num;
+}
+
+function readOper(oper, button) {
+    oper = button.textContent;
+    return oper;
+}
+
 const numButtons = document.querySelectorAll('#number');
 const operButtons = document.querySelectorAll('#operator');
 
-let firstNum = 0;
+let num = 0;
+let nextNum = 0;
+let oper = '';
+let nextOper = '';
+let clicked1 = false;
+let clicked2 = false;
 
 numButtons.forEach(button => {
     button.addEventListener('click', () => {
-        firstNum *= 10;
-        firstNum += Number(button.textContent);
-        console.log(firstNum);
+
+        if (clicked1 == false) {
+            num = readNum(num, button);
+        }
+
+        else if (clicked2 == false) {
+            nextNum = readNum(nextNum, button);
+        }
+
+        
+    });
+});
+
+operButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (clicked1 == false) {
+            oper = readOper(oper, button);
+            clicked1 = true;
+        }
+
+        else if (clicked2 == false) {
+            nextOper = readOper(nextOper, button);
+            clicked2 = true;
+
+            console.log('Num1 = ' + num);
+            console.log('Num2 = ' + nextNum);
+            console.log('Oper1 = ' + oper);
+            console.log('Oper2 = ' + nextOper);
+
+        }
+
+        
     })
-})
+});
+
 
 function operate(sign, a, b) {
+    if (sign = '+') {
+        return add(a, b);
+    }
+    
+    else if (sign = '-') {
+        return substract(a, b);
+    }
+
+    else if (sign = '*') {
+        return multiply(a, b);
+    }
+
+    else if (sign = '/') {
+        return divide(a, b);
+    }
 
 }
